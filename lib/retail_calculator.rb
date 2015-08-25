8c52b6370beb8dc8cb13cbbfae5af925ec36ef1c
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-08-25 21:55:44 +0200

    Done 5 iterations of 8 minutes each

--- lib/retail_calculator.rb
+++ lib/retail_calculator.rb
@@ -1,2 +1,35 @@
 class RetailCalculator
+
+  STATE_TAXES = {
+    'UT' => 0.0685
+  }
+
+  def self.calculate(items, item_price = 1, state_code = '')
+    total = items * item_price
+    discount = discount_for(total)
+    tax = state_tax(state_code)
+    result = total * (1.0 - discount)
+    result * (1.0 - tax)
+  end
+
+  def self.discount_for(amount)
+    case amount
+    when 0..1000
+      0.00
+    when 1001..5001
+      0.03
+    when 5001..10000
+      0.05
+    when 10001..50000
+      0.10
+    when 50001..10000000
+      0.15
+    else
+      0.15
+    end
+  end
+
+  def self.state_tax(state_code)
+    STATE_TAXES[state_code] || 0.0
+  end
 end
--- spec/retail_calculator_spec.rb
+++ spec/retail_calculator_spec.rb
@@ -2,4 +2,51 @@ require 'spec_helper'
 require 'retail_calculator'
 
 RSpec.describe RetailCalculator do
+
+  def calculate(*args)
+    RetailCalculator.calculate(*args)
+  end
+
+  describe "Simple Inputs" do
+
+    it "1 item => output 1" do
+      expect(calculate(1)).to eq(1)
+    end
+
+    it "3 items => output 3" do
+      expect(calculate(3)).to eq(3)
+    end
+
+    it "3 items x 1$ price => output 3$" do
+      expect(calculate(3, 1)).to eq(3)
+    end
+
+    it "3 items x 5$ price => output 15$" do
+      expect(calculate(3, 5)).to eq(15)
+    end
+  end
+
+  describe "Discounts" do
+
+    it "10 items x 200$ price => 3% discount for > $1000" do
+      expected_result = (10 * 200) * 0.97
+      expect(calculate(10, 200)).to eq(expected_result)
+    end
+
+    it "100 items x 600$ price => 5% discount for > $5000" do
+      expected_result = (100 * 600) * 0.95
+      expect(calculate(100, 600)).to eq(expected_result)
+    end
+  end
+
+  describe "Taxes" do
+
+    it "3 items x 5$ price x UT state 6.85% => output 13.9725$" do
+      expected_result = (3 * 5) * (1 - 0.0685)
+      expect(calculate(3, 5, 'UT')).to eq(expected_result)
+    end
+  end
+
+  describe "Discounts + Taxes" do
+  end
 end
